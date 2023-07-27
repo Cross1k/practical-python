558fddc9aa7a5bc495a40a209affc51334b51647
Cross1k <49199383+Cross1k@users.noreply.github.com>
2023-07-27 12:15:30 +0000

    excersice 1.11

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,6 +6,7 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0
 month = 0
+over_paid = 0
 
 extra_payment_start_month = 61
 extra_payment_end_month = 108
@@ -20,6 +21,11 @@ while principal > 0:
         principal = principal - extra_payment
         total_paid = total_paid + extra_payment
     print(month, round(total_paid, 2), round(principal, 2))
+else:
+    over_paid = principal
+    total_paid = total_paid + over_paid
+    print('over_paid', round(-over_paid, 2))
+    principal = 0
 
 print('total_paid', round(total_paid,2))
 print('Month', month)
